512198a3ee903edb97a771cc580f01093b7f071b
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-05-12 16:13:50 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h1>Hello world!</h1>
+  </main>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
