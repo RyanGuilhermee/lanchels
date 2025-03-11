b9cd0e642c0ef94489a20ecc872276d006673dc1
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2025-03-11 19:02:46 -0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,14 @@ let totalPix = ref(0);
 let totalMoney = ref(0);
 let totalAll = ref(0);
 
+let totalPixDonation = ref(0);
+let totalMoneyDonation = ref(0);
+let totalAllDonation = ref(0);
+
+let totalPixAll = ref(0);
+let totalMoneyAll = ref(0);
+let totalAllAll = ref(0);
+
 onMounted(async () => {
   try {
     const ordersData = await findAllOrders();
@@ -14,7 +22,7 @@ onMounted(async () => {
     //filter all order paid
     const allOrderPaid = ordersData.filter((order) => order.totalPaid);
 
-    allOrderPaid.filter((order) => {
+    allOrderPaid.forEach((order) => {
       for (const snack of order.snacks) {
         if (snack.quantityPaid) {
           if (snack.paymentMethod === 'pix') {
@@ -27,10 +35,14 @@ onMounted(async () => {
         }
       }
 
-      totalPix.value += order.donation.donationMethod === 'pix' ? order.donation.totalDonation : 0;
-      totalMoney.value += order.donation.donationMethod === 'money' ? order.donation.totalDonation : 0;
-      totalAll.value += order.donation.totalDonation;
+      totalPixDonation.value += order.donation.donationMethod === 'pix' ? order.donation.totalDonation : 0;
+      totalMoneyDonation.value += order.donation.donationMethod === 'money' ? order.donation.totalDonation : 0;
+      totalAllDonation.value += order.donation.totalDonation;
     });
+
+    totalPixAll.value = totalPix.value + totalPixDonation.value;
+    totalMoneyAll.value = totalMoney.value + totalMoneyDonation.value;
+    totalAllAll.value = totalAll.value + totalAllDonation.value;
   } catch (error) {
     console.log(error);
   }
@@ -46,8 +58,9 @@ onMounted(async () => {
     <div class="container-fluid">
       <h1>Bem-vindo ao painel de administração do <span>Lanchels!</span></h1>
 
-      <section id="incommings">
-        <h2>Entradas do dia:</h2>
+      <div class="cards-container">
+        <section id="incommings">
+        <h2>Entradas de vendas:</h2>
 
         <div class="cards d-flex justify-content-center">
           <div class="row">
@@ -94,7 +107,108 @@ onMounted(async () => {
             </div>
           </div>
         </div>
-      </section>
+        </section>
+
+        <section id="donations-incommings">
+          <h2>Entradas de doações:</h2>
+
+          <div class="cards d-flex justify-content-center">
+            <div class="row">
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Pix</h5>
+                    <p class="card-text">
+                      {{ totalPixDonation.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Dinheiro</h5>
+                    <p class="card-text">
+                      {{ totalMoneyDonation.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Total</h5>
+                    <p class="card-text">
+                      {{ totalAllDonation.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="all-incommings">
+          <h2>Entradas totais:</h2>
+
+          <div class="cards d-flex justify-content-center">
+            <div class="row">
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Pix</h5>
+                    <p class="card-text">
+                      {{ totalPixAll.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Dinheiro</h5>
+                    <p class="card-text">
+                      {{ totalMoneyAll.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div
+                  class="card text-center mb-3 shadow p-3 mb-1 bg-body-tertiary rounded"
+                  style="width: 18rem"
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">Total</h5>
+                    <p class="card-text">
+                      {{ totalAllAll.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
 
       <section id="actions">
         <div class="d-flex justify-content-center mt-5">
@@ -129,7 +243,7 @@ main h1 span {
   font-size: 30px;
 }
 
-main #incommings h2 {
+main  h2 {
   font-size: 11px;
   text-align: center;
   margin: 40px;
@@ -159,7 +273,6 @@ main #actions button {
   main .cards .row {
     display: flex;
     flex-direction: column;
-    align-content: center;
   }
 }
 
@@ -173,7 +286,7 @@ main #actions button {
     font-size: 34px;
   }
 
-  main #incommings h2 {
+  main h2 {
     font-size: 16px;
   }
 }
